ab8b5df839898888da66c07ee0e0492318ec1f5f
Magnus <Magnus@LAPTOP-J27TRD47>
2017-10-11 09:53:06 +0200

    123123

--- UdpBroadcastCapture/Program.cs
+++ UdpBroadcastCapture/Program.cs
@@ -15,29 +15,19 @@ namespace UdpBroadcastCapture
         private static readonly IPAddress IpAddress = IPAddress.Any;
         // Listen for activity on all network interfaces
         // https://msdn.microsoft.com/en-us/library/system.net.ipaddress.ipv6any.aspx
+
+
         static void Main()
         {
-            IPEndPoint remoteEndPoint = new IPEndPoint(IpAddress, 9998);
-
-            using (UdpClient socket = new UdpClient(Port))
+            using (var service = new Broadcast.Service1Client())
             {
-                while (true)
-                {
-                    Console.WriteLine("Waiting for broadcast {0}", socket.Client.LocalEndPoint);
-                    byte[] datagramReceived = socket.Receive(ref remoteEndPoint);
-
-                    string message = Encoding.ASCII.GetString(datagramReceived, 0, datagramReceived.Length);
-                    Console.WriteLine("Receives {0} bytes from {1} port {2} message {3}", datagramReceived.Length,
-                        remoteEndPoint.Address, remoteEndPoint.Port, message);
-                    //Parse(message);
-                }
+                service.PostTempToList(ref)
             }
-        }  
+        }
 
         // To parse data from the IoT devices in the teachers room, Elisagårdsvej
         private static void Parse(string response)
         {
-
             string[] parts = response.Split(' ');
             foreach (string part in parts)
             {
@@ -46,6 +36,26 @@ namespace UdpBroadcastCapture
             string temperatureLine = parts[6];
             string temperatureStr = temperatureLine.Substring(temperatureLine.IndexOf(": ") + 2);
             Console.WriteLine(temperatureStr);
+        }
+
+
+
+
+
+
+
+        public string GetBroadcast()
+        {
+            IPEndPoint remoteEndPoint = new IPEndPoint(IpAddress, 9998);
+
+            using (UdpClient socket = new UdpClient(Port))
+            {
+            
+                byte[] datagramReceived = socket.Receive(ref remoteEndPoint);
+
+                string message = Encoding.ASCII.GetString(datagramReceived, 0, datagramReceived.Length);
+                return message;
+            }
         }
     }
 }
